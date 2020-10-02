1a689a3f58d7fa2f01a04644691bc48e633e01fd
kyle-mcnaught <60240503+kyle-mcnaught@users.noreply.github.com>
2020-10-02 18:14:15 -0400

    Added moving platforms
    Created a platform that moves up and down and an obsticle to high for the player to reach without pltform.

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+
+	public float speed = 0;
+
+	public float upperLimit = 4.0f;
+	public float lowerLimit = 1.0f;
+	
+	private int direction = 1;
+
+    void Update()
+    {
+        if (transform.position.y > upperLimit) {
+         direction = -1;
+     }
+     else if (transform.position.y < lowerLimit) {
+         direction = 1;
+     }
+     Vector3 movement = Vector3.up * direction * speed * Time.deltaTime; 
+     transform.Translate(movement); 
+    }
+}
